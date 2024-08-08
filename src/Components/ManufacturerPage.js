9fec4821917a7357b587fff41333355a4affd5dd
brandondando <dandobrandon0@gmail.com>
2024-08-08 20:07:13 +0300

    Added styling for the Manufacturers page

--- src/Components/ManufacturerPage.js
+++ src/Components/ManufacturerPage.js
@@ -2,32 +2,18 @@ import React, { useState } from 'react';
 import './ManufacturerPage.css'; // For styling
 
 function ManufacturerPage() {
-  const [menuVisible, setMenuVisible] = useState(true);
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
-
   return (
-    <div className="manufacturer-container"><br />
-      <button className="menu-toggle" onClick={toggleMenu}>
-        {menuVisible ? 'Hide Menu' : 'Show Menu'}
-      </button>
-      {menuVisible && (
-        <aside className="side-menu">
-            <br /><br />
-          <h2>Manufacturer Menu</h2>
-          <ul>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Orders From Admin</a></li>
-            <li><a href="#">Logout</a></li>
-          </ul>
-        </aside>
-      )}
+    <div className="manufacturer-container">
+      <aside className="side-menu">
+        <h2>Manufacturer Menu</h2>
+        <ul>
+          <li><a href="#">Orders From Admin</a></li>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </aside>
       <main className="main-content">
         <h1>Manufacturer Dashboard</h1>
-
-
       </main>
     </div>
   );
