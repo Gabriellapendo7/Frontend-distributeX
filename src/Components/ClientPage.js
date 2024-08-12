c11576c3c50d11004191ba583edc82e930dafaec
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-08-12 09:11:38 +0300

    Merged from djBrandy

--- src/Components/ClientPage.js
+++ src/Components/ClientPage.js
@@ -7,24 +7,35 @@ import './ClientPage.css';
 
 
 const products = [
-  { description: 'Nyayo Beans 1kgx12', price: 'Ksh.3,300', img: 'https://cdn.mafrservices.com/sys-master-root/h27/hff/12454522486814/22053_Main.jpg?im=Resize=1700?im=Resize=400' },
-  { description: 'Krackles Assorted flavoursx24', price: 'Ksh.1,400', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/4oti6KlZHiRZucaycSyTEffux9K_Jo6EdRRnNjU4LCs.jpg' },
+  // pantry
   { description: 'Cereal Corn Flakes Value Pack 1Kgx12', price: 'Ksh.5,500', img: 'https://cdn.mafrservices.com/sys-master-root/h9b/h44/50586219184158/19714_main.jpg?im=Resize=1700?im=Resize=400' },
   { description: 'Tilly Cooking Fat 10kg', price: 'Ksh.2,400', img: 'https://cdn.mafrservices.com/sys-master-root/h43/hd4/26950721732638/21031_1.jpg?im=Resize=1700?im=Resize=400' },
   { description: 'Rina Vegetable Cooking oil 20L', price: 'Ksh.4,000', img: 'https://cdn.mafrservices.com/sys-master-root/hb7/h7f/17384319385630/21020_main.jpg?im=Resize=1700?im=Resize=400' },
+  { description: 'Nyayo Beans 1kgx12', price: 'Ksh.3,300', img: 'https://cdn.mafrservices.com/sys-master-root/h27/hff/12454522486814/22053_Main.jpg?im=Resize=1700?im=Resize=400' },
   { description: 'Captain Cook Corn Oil', price: 'Ksh.7,000', img: 'https://cdn.mafrservices.com/sys-master-root/hba/h28/50586218070046/20991_main.jpg?im=Resize=1700?im=Resize=400' },
-  { description: 'Brookside Dairy milk 500mlx12', price: 'Ksh.600', img: 'https://mybigorder.com/public/uploads/products/photos/DNf8oAIUDWYuYFVuXrc0lpjWhR6OakhTkn2kPzzv.jpeg' },
   { description: 'Royco Mchuzi Mix 2kg', price: 'Ksh.500', img: 'https://kikuuboonline.ams3.cdn.digitaloceanspaces.com/Items/637904420592676604.png' },
   { description: 'Royco Mchuzi Mix 170gx24', price: 'Ksh.1,900', img: 'https://cdn.mafrservices.com/sys-master-root/h26/h79/50428192489502/13913_3.jpg?im=Resize=1700?im=Resize=400' },
   { description: 'Blueband Croma For Catering Medium Fat Spread 10kg', price: 'Ksh.3,000', img: 'https://cdn.mafrservices.com/sys-master-root/h30/hf9/12451482730526/13872_Main.jpg?im=Resize=480' },
-  { description: 'Oreo Biscuit 55gx12', price: 'Ksh.600', img: 'https://cdn.mafrservices.com/sys-master-root/hed/h91/51430620495902/182716_main.jpg?im=Resize=480' },
   { description: 'Kakira Light Brown Sugar 5kg', price: 'Ksh.1,200', img: 'https://cdn.mafrservices.com/sys-master-root/h09/h99/17328354885662/68475_main.jpg?im=Resize=480' },
   { description: 'Prestige Margarine 5KG', price: 'Ksh.2,000', img: 'https://cdn.mafrservices.com/sys-master-root/h09/h0c/26449282859038/21010_main.jpg?im=Resize=1700?im=Resize=400' },
   { description: 'Blue Band 250gx12', price: 'Ksh.1,440', img: 'https://cdn.mafrservices.com/sys-master-root/he9/h90/30801988091934/13869_main.jpg?im=Resize=480' },
   { description: 'Choco Primo Drinking Chocolate Powder 20gx36', price: 'Ksh.540', img: 'https://cdn.mafrservices.com/sys-master-root/hb5/hab/51467885477918/211007_main.jpg?im=Resize=480' },
-  { description: 'Tropical Heat Kenyan Chevda Hot 50gx24', price: 'Ksh.1,200', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/mEih5FmPYOAFvhXcGbgj5h4uY0gtcoo35TQDJLZ58-Y.png' },
   { description: 'Indomie Instant Noodles Beef 20x120g', price: 'Ksh.800', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/16/LLHd1pUX4HzFk96-4RNjsYu2KTBbcFTdmkWkm8ejFdA.png' },
   { description: 'Noodies Instant Noodles Beef 5Pack 8x70g', price: 'Ksh.850', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/15/4hQITDUMRYokDIsv-XYJZI4vCLmmPtHpwpUstX5cINM.jpg' },
+  // snacks
+  { description: 'Krackles Assorted flavoursx24', price: 'Ksh.1,400', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/4oti6KlZHiRZucaycSyTEffux9K_Jo6EdRRnNjU4LCs.jpg' },
+  
+  // drinks
+  // toiletries
+  // dairy
+  { description: 'Brookside Dairy milk 500mlx12', price: 'Ksh.600', img: 'https://mybigorder.com/public/uploads/products/photos/DNf8oAIUDWYuYFVuXrc0lpjWhR6OakhTkn2kPzzv.jpeg' },
+ 
+  
+ 
+  { description: 'Oreo Biscuit 55gx12', price: 'Ksh.600', img: 'https://cdn.mafrservices.com/sys-master-root/hed/h91/51430620495902/182716_main.jpg?im=Resize=480' },
+  
+  { description: 'Tropical Heat Kenyan Chevda Hot 50gx24', price: 'Ksh.1,200', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/mEih5FmPYOAFvhXcGbgj5h4uY0gtcoo35TQDJLZ58-Y.png' },
+  
   { description: 'Pick N Peel Pure Fruit Juice Tetra Apple 12x1L', price: '2,500', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/FEeeul3NxQClK1tKWjqpvctMFZ5KtRtU3B0ZL0Kopnw.jpg' },
   { description: 'Tropical Heat Potato Crisps Cheese & Onion 200gx12', price: '500', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/17/wzTx56a0J_b-BU_DezBZ4Iae855eYJ_syyTtgIdQYaE.jpg' },
   { description: 'Celine Premium Toilet Tissue 10x4s', price: 'Ksh.300', img: 'https://cdn.optipic.io/site-2766/images/thumbnails/550/450/detailed/16/6n6VPvUWCO7FOp3A_Cm_PTF68av8M0yYP1rRWq8VEDg.jpg' },
